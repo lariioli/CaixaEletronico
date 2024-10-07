e90116c76067b4048f5e461d702915dd679d4072
larissa de oliveira <larissaoliveira3zb@gmail.com>
2024-10-07 16:38:21 -0300

    CaixaEletronico

--- Program.cs
+++ Program.cs
@@ -5,11 +5,14 @@ class Program
 { 
     static void Main(string[] args)
     {
-    Console.Write("Digite o nome do titular da conta: ");
-    string titular = Console.ReadLine();
+        Console.Write("Digite o nome do titular da conta: ");
+        string titular = Console.ReadLine();
 
-    Console.Write("Digite o tipo de conta (Corrente/Poupança): ");
-    string tipoConta = Console.ReadLine();
+        Console.Write("Digite o tipo de conta (Corrente/Poupança): ");
+        string tipoConta = Console.ReadLine();
 
-    Console.Write("Crie uma senha para sua conta: ");
-    string senha = 
+        Console.Write("Crie uma senha para sua conta: ");
+        string senha = Console.ReadLine();
+
+        Console.Write("Digite o número da conta: ");
+        string numeroConta = Console.ReadLine();
