c5fbfc6716ecc3985cba0187dc5aa820b3fd3ed6
larissa de oliveira <larissaoliveira3zb@gmail.com>
2024-10-07 16:35:04 -0300

    CaixaEletronico

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;
+class Program
+{
+  static void Main(string[] args)
+ {
+    Console.Write("Digite o nome do titular da conta: ");
+    string titular = Console.ReadLine();
+
+    Console.Write("Digite o tipo de conta (Corrente/Poupança): ");
+    string tipoConta = Console.ReadLine();
+
+    
